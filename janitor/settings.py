ea8b7e48ca1888d0c1a4d527592ca3179dcebd00
Murat Eken <meken@google.com>
2026-06-03 13:52:22 +0200

    upgrading to ADK 2 and Gemini 3.5...

--- janitor/settings.py
+++ janitor/settings.py
@@ -1,1 +1,13 @@
-GEMINI_MODEL = "gemini-2.5-flash"
+from functools import cached_property
+
+from google.adk.models import Gemini
+from google.genai import Client
+
+
+class GlobalGemini(Gemini):
+  """Utility class to use the global region for the Gemini models."""
+  @cached_property
+  def api_client(self) -> Client:
+    return Client(location="global")
+
+GEMINI_MODEL=GlobalGemini(model="gemini-3.5-flash")
